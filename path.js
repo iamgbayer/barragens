2d34ca3fd49ce678c4744992f9285a1fc9505b6d
guuibayer <guiilherme.bayer@gmail.com>
2019-01-30 13:28:33 -0200

    Fixing path from bundle file

--- path.js
+++ path.js
@@ -2,6 +2,6 @@ const { join } = require('path')
 
 module.exports = {
   source: join(process.cwd(), `src`),
-  dist: join(process.cwd(), 'dist'),
+  dist: join(process.cwd(), 'build'),
   public: join(process.cwd(), 'public')
 }
