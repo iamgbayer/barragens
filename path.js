71ce11d34c0ff7e922e4a1472c55be5e1239adb8
guuibayer <guiilherme.bayer@gmail.com>
2019-01-31 10:17:19 -0200

    Adding data csv and config to relocate the file during build

--- path.js
+++ path.js
@@ -3,5 +3,6 @@ const { join } = require('path')
 module.exports = {
   source: join(process.cwd(), `src`),
   build: join(process.cwd(), 'build'),
-  public: join(process.cwd(), 'public')
+  public: join(process.cwd(), 'public'),
+  data: join(process.cwd(), 'data/data.csv')
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,19 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const path = require('./path')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.data,
+        to: path.build
+      }
+    ])
+  ]
 })
